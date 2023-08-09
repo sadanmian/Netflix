94392ab256037ba8d59b9d6e2b248b2b93bdcfb4
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-09 21:24:33 +0530

    list

---
+++ src/components/list/List.jsx
@@ -0,0 +1,55 @@
+import React, { useRef, useState } from "react";
+import "./list.scss";
+import {
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from "@mui/icons-material";
+import { ListItem } from "../listItem/ListItem";
+
+export const List = () => {
+  const [slideNumber, setSlideNumber] = useState(0);
+  const [isMoved, setIsMoved] = useState(false);
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+    }
+    if (direction === "right" && slideNumber < 4) {
+      setSlideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+    }
+    console.log(distance);
+  };
+  return (
+    <div className="list">
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlined
+          className="sliderArrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+        </div>
+        <ArrowForwardIosOutlined
+          className="sliderArrow right"
+          onClick={() => handleClick("right")}
+        />
+      </div>
+    </div>
+  );
+};
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import "./home.scss";
 import { Navbar } from "../components/navbar/Navbar";
 import { Featured } from "../components/featured/Featured";
+import { List } from "../components/list/List";
 
 export const Home = () => {
   return (
     <div className="home">
       <Navbar />
       <Featured />
+      <List />
+      <List />
+      <List />
+      <List />
     </div>
   );
 };
