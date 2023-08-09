e7b35252d14743979d58a85842ec5e381d68b057
Sadan Mian <sadanmiyan007@gmail.com>
2023-08-09 23:07:30 +0530

    Register Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Home } from "./pages/home/Home";
 import "./app.scss";
 import { Watch } from "./pages/watch/Watch";
+import { Register } from "./pages/register/Register";
 
 function App() {
   return (
     <div className="App">
       {/* <Home /> */}
-      <Watch />
+      {/* <Watch /> */}
+      <Register />
     </div>
   );
 }
